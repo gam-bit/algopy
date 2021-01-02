d470b0854bcce3a8dbb8b23984b5c184c9a210ec
mzkang <gam_bit91@naver.com>
2021-01-02 23:25:30 +0900

    2021-01-02 | dp | update

--- Codes/makeone.py
+++ Codes/makeone.py
@@ -11,45 +11,38 @@
 # 3
 #--------------------------------------------------
 
-# 재귀
-def makeone(n, cnt = 0):
-    
-    if n == 1:
-        return cnt
-
-    if n % 5 == 0:
-        return makeone(n//5, cnt+1)
-    
-    elif n % 3 == 0:
-        return makeone(n//3, cnt+1)
-    
-    elif n % 2 == 0:
-        return makeone(n//2, cnt+1)
-
-    return makeone(n-1, cnt+1)
-
-
-# 2) dp
+# 주의!!! 
+# dp
 def makeone_dp(n):
     
-    memo = {}
-    memo[1], memo[2], memo[3], memo[4], memo[5] = 1, 1, 1, 2, 1
+    memo = [0] * (n+1)
+    # memo의 index가 우리가 나눌 값
+    # 1은 연산 횟수가 0이므로 따로 처리할 필요가 없음
     
-    for i in range(6, n+1):
+    for i in range(2, n+1):
+        lst = [] # 비교할 리스트
+
         if i % 5 == 0:
-            memo[i] = memo[i//5] + 1
+            find_idx = i//5
+            lst.append(memo[find_idx] + 1)
         elif i % 3 == 0:
-            memo[i] = memo[i//3] + 1 
+            find_idx = i//3
+            lst.append(memo[find_idx] + 1)
         elif i % 2 == 0:
-            memo[i] = memo[i//2] + 1
-        else:
-            memo[i] = memo[i-1] + 1
+            find_idx = i//2
+            lst.append(memo[find_idx] + 1)
+        
+        find_idx = i-1
+        lst.append(memo[find_idx] + 1)
+        
+        memo[i] = min(lst)
+
     print(memo) 
     return memo[n]
 
     
 x = int(input())
 
-print(makeone(x))
+print(makeone_dp(x))
 
 
