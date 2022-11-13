38a7ab9096b9ee08781daa98b9f38ac085c4e81f
gam_bit91 <gam_bit91@naver.com>
2022-11-13 22:20:25 +0900

    bfs | 미로 관련 문제는 dfs/bfs로 풀기

---
+++ Codes/2022/04_DFS_BFS/221113_프로그래머스_lv2_게임 맵 최단거리_retry.py
@@ -0,0 +1,38 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/1844
+from collections import deque
+
+def solution(maps):
+    def bfs(y, x):
+        q = deque()
+        q.append((y, x, 1))
+        
+        while q:
+            y, x, distance = q.popleft()    
+            if y == len(maps)-1 and x == len(maps[0])-1:
+                return distance
+            
+            if maps[y][x] == 0:
+                continue
+            maps[y][x] = 0
+            
+            if y+1 < len(maps):
+                q.append((y+1, x, distance+1))
+            if y-1 >= 0:
+                q.append((y-1, x, distance+1))
+            if x+1 < len(maps[0]):
+                q.append((y, x+1, distance+1))
+            if x-1 >= 0:
+                q.append((y, x-1, distance+1))
+        return -1
+    return bfs(0, 0)
+
+
+if __name__=="__main__":
+    maps = [[1,0,1,1,1],
+            [1,0,1,0,1],
+            [1,0,1,1,1],
+            [1,1,1,0,1],
+            [0,0,0,0,1]] # 11
+    # maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]] # -1
+    print(solution(maps))
+    # print(maps[1][0])
