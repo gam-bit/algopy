35eab4f9ee45efe575158822e9336e65af17ab6f
gam_bit91 <gam_bit91@naver.com>
2023-10-11 00:30:40 +0900

    Modified code. 정답 아님.

--- Codes/2023/하반기 코딩테스트/20057_백준_마법사 상어와 토네이도_골드3.py
+++ Codes/2023/하반기 코딩테스트/20057_백준_마법사 상어와 토네이도_골드3.py
@@ -21,30 +21,70 @@ def rotate90(proportion):
     new_p = list(zip(*proportion))[::-1]
     return new_p
 
+def main(n, graph):
+    p = [[0, 0, 0.02, 0, 0],
+         [0, 0.1, 0.07, 0.01, 0],
+         [0.05, 0, 0, 0, 0], # [0.05, 남은 모래, 도착, 출발, 0]
+         [0, 0.1, 0.07, 0.01, 0],
+         [0, 0, 0.02, 0, 0]]
 
-n = 7
-i, j = ((n-1)/2, (n-1)/2)
+    x_i, x_j = (int((n-1)/2), int((n-1)/2)) # x : 현 위치 
 
-m = 0
-m_value = 1
-move_idx = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-
-print((i, j))
-while True:
-    if i <= 0 and j <= 0:
-        raise ValueError
-
-    k = m % 4 
-    move_i, move_j = move_idx[k]
-
-    i += move_i*m_value
-    j += move_j*m_value
+    m = 0
+    m_value = 1
+    move_idx = [(0, -1), (1, 0), (0, 1), (-1, 0)]
     
+    sand_out = 0
     
+    while True:
 
-    m += 1
-    if k == 1 or k == 3:
-        m_value += 1
+        k = m % 4 
+        move_i, move_j = move_idx[k]
 
-    print((i, j), m_value)
+        # y의 모래 양 확인
+        for _ in range(m_value):
+            y_i = x_i + move_i
+            y_j = x_j + move_j
+            if y_i < 0 or y_j < 0 or y_i >= n or y_j >= n:
+                return sand_out
+            
+            sand_y = graph[y_i][y_j]
+
+            if sand_y == 0:
+                x_i, x_j = y_i, y_j
+                continue
+
+            graph[y_i][y_j] = 0
+
+            sand_move = [[int(i*sand_y) for i in l] for l in p]
+            sand_move_total = sum([sum(i) for i in sand_move])
+            alpha = sand_y - sand_move_total
+            sand_move[2][1] = alpha
+            
+            # 모래 이동
+                # 기준점 (2, 2) = y 위치
+                # 범위 (0,0) ~ (4, 4)
+            for a in range(-2, 3):
+                for b in range(-2, 3):
+                    sand = sand_move[2+a][2+b]
+                    if sand:
+                        check_i, check_j = y_i+a, y_j+b
+                        if check_i >= 0 and check_i < n and check_j >=0 and check_j < n:
+                            graph[check_i][check_j] += sand
+                        else:
+                            sand_out += sand
+
+            x_i, x_j = y_i, y_j
+
+        p = rotate90(p)
+    
+        m += 1
+        if k == 1 or k == 3:
+            m_value += 1
+
+
+if __name__=="__main__":
+    n = int(input())
+    graph = [[int(i) for i in input().split(' ')] for _ in range(n)]
+
+    print(main(n, graph))
